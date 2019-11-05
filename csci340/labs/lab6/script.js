040db7454ec3ac5e5a1ec41eface65d43192cb5d
Mitchell Rotenberry <mlrotenberry99@yahoo.com>
2019-11-04 21:28:20 -0600

    finished my jquery website

--- csci340/labs/lab6/script.js
+++ csci340/labs/lab6/script.js
@@ -1,23 +1,72 @@
 $(document).ready(function(){
- $("#testing").on("click", function (){
+  var cats = true;
+  var dogs = false;
+  $.ajax({
+    dataType: "json",
+    url: "https://aws.random.cat/meow",
+    success: function(result) {
+      $("#pic").attr("src", result["file"]);
+    },
+    error: function(xhr,status,error) {
+       console.log(error);
+   }
+ });
+ $.ajax({
+   url: "https://api.kanye.rest",
+   success: function (result) {
+     $("#kanye").text(result["quote"]);
+   },
+   error: function(xhr,status,error) {
+         console.log(error);
+  }
+ });
+
+$("#cat").on("click", function () {
+  cats = true;
+  dogs = false;
+});
+
+$("#dog").on("click", function () {
+  cats = false;
+  dogs = true;
+});
+
+ $("#clicker").on("click", function (){
    $.ajax({
      url: "https://api.kanye.rest",
      success: function (result) {
-       $("#testing").text(result["quote"]);
+       $("#kanye").text(result["quote"]);
      },
      error: function(xhr,status,error) {
            console.log(error);
     }
    });
-   $.ajax({
-     ult: "https://pixabay.com/api/?key=14140721-b59535f9f2cb06a4c44807198&category=travel",
+   if (cats){
+     $.ajax({
+       dataType: "json",
+       url: "https://aws.random.cat/meow",
+       success: function(result) {
+         $("#pic").attr("src", result["file"]);
+       },
+       error: function(xhr,status,error) {
+          console.log(error);
+      }
+    });
+  } else if (dogs){
+    $.ajax({
      dataType: "json",
+     url: "https://random.dog/woof.json",
      success: function(results) {
-       $("#pix").attr("src", results["largeImageURL"])
+       if (results["url"].endsWith(".mp4")) {
+          $('#pic').attr("src", "https://i.ytimg.com/vi/OOFGdRmN70k/maxresdefault.jpg");
+        } else {
+          $('#pic').attr("src", results["url"]);
+        }
      },
      error: function(xhr,status,error) {
-        console.log(error);
-    }
-  });
+       console.log(error);
+     }
+   });
+  }
  });
 });
